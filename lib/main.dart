bb6eae670444386b9d1492765ea967df0ef14247
Marko Mijić <markomijic4@gmail.com>
2026-06-06 09:49:22 +0200

    Updated pubspec, removed service

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart' show dotenv;
 import 'package:hive_ce_flutter/hive_flutter.dart' show Hive, HiveX;
 import 'package:hooks_riverpod/hooks_riverpod.dart' show ProviderScope;
 import 'package:package_info_plus/package_info_plus.dart' show PackageInfo;
 import 'package:window_manager/window_manager.dart'
     show WindowOptions, windowManager;
-
-import 'src/app.dart';
+import 'src/app.dart' show App;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await windowManager.ensureInitialized();
-  try {
-    await dotenv.load(fileName: '.env');
-  } catch (_) {}
-
   await Hive.initFlutter('TTS Mod Vault');
 
   final packageInfo = await PackageInfo.fromPlatform();
